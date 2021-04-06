939928ad73cedc49aa25d06af84cde8c0d1a100b
Germo Görtz <github.com@aisberg.de>
2021-04-06 14:51:55 +0200

    docs IndexSemanticGroup

---
+++ db_DataHandwerk/docs/Views/AntoraNavListPage_by_type.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE VIEW [docs].[AntoraNavListPage_by_type]
+AS
+SELECT type
+ , type_desc
+ , nav_list = CONCAT (
+  '= '
+  , type_desc
+  , CHAR(13) + CHAR(10)
+  , CHAR(13) + CHAR(10)
+  , 'include::partial$navlist/navlist-type-' + type + '.adoc[]'
+  , CHAR(13) + CHAR(10)
+  )
+FROM config.type
+WHERE (is_DocsOutput = 1)
---
+++ db_DataHandwerk/docs/Views/AntoraNav_by_type.sql
@@ -0,0 +1,38 @@
+﻿
+
+/*
+output example:
+
+* Objects by type
+** xref:nav/nav-type-U.adoc[]
++
+--
+include::partial$navlist/navlist-type-U.adoc[]
+--
+
+*/
+CREATE VIEW [docs].[AntoraNav_by_type]
+AS
+SELECT partial_content = CONCAT (
+  '* Objects by type'
+  , CHAR(13) + CHAR(10)
+  , STRING_AGG(CONCAT (
+    '** xref:nav/nav-type-' + ct.type + '.adoc[]'
+    , CHAR(13) + CHAR(10)
+    , '+'
+    , CHAR(13) + CHAR(10)
+    , '--'
+    , CHAR(13) + CHAR(10)
+    , 'include::partial$navlist/navlist-type-' + ct.type + '.adoc[]'
+    , CHAR(13) + CHAR(10)
+    , '--'
+    ), CHAR(13) + CHAR(10)) WITHIN GROUP (
+    ORDER BY ct.type_desc desc
+    )
+  )
+FROM config.type ct
+--only existing objects
+INNER JOIN [docs].[AntoraNavListRepoObject_by_type] existing
+ ON existing.type = ct.type
+GROUP BY is_DocsOutput
+HAVING (is_DocsOutput = 1)
