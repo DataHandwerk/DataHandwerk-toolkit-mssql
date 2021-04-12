fb9be025abbb6bf4e068840ac649125fc1843e38
Germo Görtz <github.com@aisberg.de>
2021-04-12 18:50:28 +0200

    refactoring

--- db_DataHandwerk/repo/Stored Procedures/usp_connect_database.sql
+++ db_DataHandwerk/repo/Stored Procedures/usp_connect_database.sql
@@ -1,33 +1,36 @@
 ﻿/*
--- tag::example[]  
+<<property_start>>exampleUsage
 EXEC [repo].[usp_connect_database]
 @dwh_database_name = 'WideWorldImporters'
--- end::example[]  
+<<property_end>>
 */
-CREATE PROCEDURE [repo].[usp_connect_database] (@dwh_database_name NVARCHAR(128))
-AS
+Create Procedure repo.usp_connect_database
+( @dwh_database_name NVarchar(128))
+As
 --
 --ensure existence of required parameters like 'dwh_database_name'
-EXEC [config].[usp_init_parameter]
+Exec config.usp_init_parameter;
 
 --ensure [repo].[spt_values] is filled, otherwise extended properties will not be written into database
-EXEC [config].[usp_init_spt_values]
+Exec config.usp_init_spt_values;
 
-DECLARE @dwh_database_name_old NVARCHAR(128) = (
-  SELECT [repo].[fs_dwh_database_name]()
-  )
+Declare @dwh_database_name_old NVarchar(128) =
+        (
+            Select repo.fs_dwh_database_name ()
+        );
 
-EXEC [repo].[usp_parameter_insert_update] @Parameter_name = 'dwh_database_name'
- , @Parameter_value = @dwh_database_name
+Exec repo.usp_parameter_set
+    @Parameter_name = 'dwh_database_name'
+  , @Parameter_value = @dwh_database_name;
 
 --this required every time, in case synonyms are corrupt or new synonyms have been added
 --IF @dwh_database_name <> ISNULL(@dwh_database_name_old , '')
-BEGIN
- DECLARE @SQLString NVARCHAR(4000)
- DECLARE @ParmDefinition NVARCHAR(500)
+Begin
+    Declare @SQLString NVarchar(4000);
+    Declare @ParmDefinition NVarchar(500);
 
- SET @SQLString = 
-  '
+    Set @SQLString
+        = N'
 DROP SYNONYM  IF EXISTS [sys_dwh].[columns]
 DROP SYNONYM  IF EXISTS [sys_dwh].[computed_columns]
 DROP SYNONYM  IF EXISTS [sys_dwh].[default_constraints]
@@ -49,36 +52,60 @@ DROP SYNONYM  IF EXISTS [sys_dwh].[sql_modules]
 DROP SYNONYM  IF EXISTS [sys_dwh].[tables]
 DROP SYNONYM  IF EXISTS [sys_dwh].[types]
 
-CREATE SYNONYM [sys_dwh].[columns] FOR [' 
-  + @dwh_database_name + '].[sys].[columns]
-CREATE SYNONYM [sys_dwh].[computed_columns] FOR [' + @dwh_database_name + '].[sys].[computed_columns]
-CREATE SYNONYM [sys_dwh].[default_constraints] FOR [' + @dwh_database_name + '].[sys].[default_constraints]
-CREATE SYNONYM [sys_dwh].[dm_exec_describe_first_result_set] FOR [' + @dwh_database_name + '].[sys].[dm_exec_describe_first_result_set]
-CREATE SYNONYM [sys_dwh].[dm_sql_referenced_entities] FOR [' + @dwh_database_name + '].[sys].[dm_sql_referenced_entities]
-CREATE SYNONYM [sys_dwh].[extended_properties] FOR [' + @dwh_database_name + '].[sys].[extended_properties]
-CREATE SYNONYM [sys_dwh].[foreign_key_columns] FOR [' + @dwh_database_name + '].[sys].[foreign_key_columns]
-CREATE SYNONYM [sys_dwh].[foreign_keys] FOR [' + @dwh_database_name + '].[sys].[foreign_keys]
-CREATE SYNONYM [sys_dwh].[identity_columns] FOR [' + @dwh_database_name + '].[sys].[identity_columns]
-CREATE SYNONYM [sys_dwh].[indexes] FOR [' + @dwh_database_name + '].[sys].[indexes]
-CREATE SYNONYM [sys_dwh].[index_columns] FOR [' + @dwh_database_name + '].[sys].[index_columns]
-CREATE SYNONYM [sys_dwh].[objects] FOR [' + @dwh_database_name + '].[sys].[objects]
-CREATE SYNONYM [sys_dwh].[parameters] FOR [' + @dwh_database_name + '].[sys].[parameters]
-CREATE SYNONYM [sys_dwh].[schemas] FOR [' + @dwh_database_name + '].[sys].[schemas]
-CREATE SYNONYM [sys_dwh].[sp_addextendedproperty] FOR [' + @dwh_database_name + '].[sp_addextendedproperty]
-CREATE SYNONYM [sys_dwh].[sp_updateextendedproperty] FOR [' + @dwh_database_name + '].[sp_updateextendedproperty]
-CREATE SYNONYM [sys_dwh].[sql_expression_dependencies] FOR [' + @dwh_database_name + '].[sys].[sql_expression_dependencies]
-CREATE SYNONYM [sys_dwh].[sql_modules] FOR [' + @dwh_database_name + '].[sys].[sql_modules]
-CREATE SYNONYM [sys_dwh].[tables] FOR [' + @dwh_database_name + '].[sys].[tables]
-CREATE SYNONYM [sys_dwh].[types] FOR [' + @dwh_database_name + '].[sys].[types]
-'
+CREATE SYNONYM [sys_dwh].[columns] FOR [' + @dwh_database_name
+          + N'].[sys].[columns]
+CREATE SYNONYM [sys_dwh].[computed_columns] FOR [' + @dwh_database_name
+          + N'].[sys].[computed_columns]
+CREATE SYNONYM [sys_dwh].[default_constraints] FOR [' + @dwh_database_name
+          + N'].[sys].[default_constraints]
+CREATE SYNONYM [sys_dwh].[dm_exec_describe_first_result_set] FOR [' + @dwh_database_name
+          + N'].[sys].[dm_exec_describe_first_result_set]
+CREATE SYNONYM [sys_dwh].[dm_sql_referenced_entities] FOR [' + @dwh_database_name
+          + N'].[sys].[dm_sql_referenced_entities]
+CREATE SYNONYM [sys_dwh].[extended_properties] FOR [' + @dwh_database_name
+          + N'].[sys].[extended_properties]
+CREATE SYNONYM [sys_dwh].[foreign_key_columns] FOR [' + @dwh_database_name
+          + N'].[sys].[foreign_key_columns]
+CREATE SYNONYM [sys_dwh].[foreign_keys] FOR [' + @dwh_database_name
+          + N'].[sys].[foreign_keys]
+CREATE SYNONYM [sys_dwh].[identity_columns] FOR [' + @dwh_database_name
+          + N'].[sys].[identity_columns]
+CREATE SYNONYM [sys_dwh].[indexes] FOR [' + @dwh_database_name
+          + N'].[sys].[indexes]
+CREATE SYNONYM [sys_dwh].[index_columns] FOR [' + @dwh_database_name
+          + N'].[sys].[index_columns]
+CREATE SYNONYM [sys_dwh].[objects] FOR [' + @dwh_database_name
+          + N'].[sys].[objects]
+CREATE SYNONYM [sys_dwh].[parameters] FOR [' + @dwh_database_name
+          + N'].[sys].[parameters]
+CREATE SYNONYM [sys_dwh].[schemas] FOR [' + @dwh_database_name
+          + N'].[sys].[schemas]
+CREATE SYNONYM [sys_dwh].[sp_addextendedproperty] FOR [' + @dwh_database_name
+          + N'].[sp_addextendedproperty]
+CREATE SYNONYM [sys_dwh].[sp_updateextendedproperty] FOR [' + @dwh_database_name
+          + N'].[sp_updateextendedproperty]
+CREATE SYNONYM [sys_dwh].[sql_expression_dependencies] FOR [' + @dwh_database_name
+          + N'].[sys].[sql_expression_dependencies]
+CREATE SYNONYM [sys_dwh].[sql_modules] FOR [' + @dwh_database_name
+          + N'].[sys].[sql_modules]
+CREATE SYNONYM [sys_dwh].[tables] FOR [' + @dwh_database_name
+          + N'].[sys].[tables]
+CREATE SYNONYM [sys_dwh].[types] FOR [' + @dwh_database_name + N'].[sys].[types]
+'   ;
 
- EXECUTE sp_executesql @SQLString
-END
+    Execute sp_executesql @SQLString;
+End;
 
-GO
-EXECUTE sp_addextendedproperty @name = N'RepoObject_guid', @value = 'b390291c-9d61-eb11-84dc-a81e8446d5b0', @level0type = N'SCHEMA', @level0name = N'repo', @level1type = N'PROCEDURE', @level1name = N'usp_connect_database';
+Go
+Execute sp_addextendedproperty
+    @name = N'RepoObject_guid'
+  , @value = 'b390291c-9d61-eb11-84dc-a81e8446d5b0'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'repo'
+  , @level1type = N'PROCEDURE'
+  , @level1name = N'usp_connect_database';
 
 
-GO
+Go
 
 
---
+++ db_DataHandwerk/repo/Stored Procedures/usp_parameter_set.sql
@@ -0,0 +1,62 @@
+﻿Create Procedure repo.usp_parameter_set
+(
+    @Parameter_name  Varchar(100)
+  , @sub_Parameter   NVarchar(128) = ''
+  , @Parameter_value Sql_Variant
+)
+As
+--
+Update
+    p
+Set
+    Parameter_value = @Parameter_value
+From
+    repo.Parameter p
+Where
+    p.Parameter_name    = @Parameter_name
+    And p.sub_Parameter = @sub_Parameter;
+
+--IF NOT EXISTS
+--(
+--    SELECT
+--           [col]
+--    FROM
+--         [repo].[Parameter] AS p
+--    WHERE  [p].[Parameter_name] = @Parameter_name
+--           AND [p].[sub_Parameter] = @sub_Parameter
+--)
+Insert Into repo.Parameter
+(
+    Parameter_name
+  , sub_Parameter
+  , Parameter_value
+)
+Select
+    @Parameter_name
+  , @sub_Parameter
+  , @Parameter_value
+Where
+    Not Exists
+(
+    Select
+        1
+    From
+        repo.Parameter As p
+    Where
+        p.Parameter_name    = @Parameter_name
+        And p.sub_Parameter = @sub_Parameter
+);
+
+Go
+Execute sp_addextendedproperty
+    @name = N'RepoObject_guid'
+  , @value = '9990291c-9d61-eb11-84dc-a81e8446d5b0'
+  , @level0type = N'SCHEMA'
+  , @level0name = N'repo'
+  , @level1type = N'PROCEDURE'
+  , @level1name = N'usp_parameter_set';
+
+
+Go
+
+
