8341e4d2eddd5fced06eb604deaf6af0b328a78f
Germo Görtz <github.com@aisberg.de>
2021-05-11 16:45:19 +0200

    use step description in docs

--- db_DataHandwerk/uspgenerator/Tables/GeneratorUsp.sql
+++ db_DataHandwerk/uspgenerator/Tables/GeneratorUsp.sql
@@ -6,9 +6,11 @@
     [usp_Description] NVARCHAR (4000) NULL,
     [usp_Examples]    NVARCHAR (4000) NULL,
     [usp_fullname]    AS              (concat('[',[usp_schema],'].[',[usp_name],']')) PERSISTED NOT NULL,
-    CONSTRAINT [PK_GeneratorUsp] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [PK_GeneratorUsp] PRIMARY KEY CLUSTERED ([id] ASC) WITH (DATA_COMPRESSION = PAGE),
     CONSTRAINT [UK_GeneratorUsp_Schema_Name] UNIQUE NONCLUSTERED ([usp_schema] ASC, [usp_name] ASC)
 );
+
+
 
 
 
