bca0eb7f5e1d269f9a840675a8c70d60915314cd
Germo Görtz <github.com@aisberg.de>
2021-07-14 15:21:02 +0200

    Property. ... ForUpdate

---
+++ db_DataHandwerk/property/Views/RepoObjectProperty_ForUpdate.sql
@@ -0,0 +1,24 @@
+﻿Create View [property].[RepoObjectProperty_ForUpdate]
+As
+Select
+    T1.[RepoObjectProperty_id]
+  , T1.[RepoObject_guid]
+  , T1.[property_name]
+  , T1.[property_value]
+  , T1.[inheritance]
+  --,T1.[property_basetype]
+  --,T1.[property_int]
+  --,T1.[property_bigint]
+  --,T1.[property_varchar]
+  --,T1.[property_nvarchar]
+  --,T1.[property_real]
+  --,T1.[property_float]
+  --,T1.[property_money]
+  , T2.RepoObject_fullname
+  , T2.RepoObject_fullname2
+From
+    [property].[RepoObjectProperty] T1
+    Inner Join
+        repo.RepoObject             T2
+            On
+            T2.RepoObject_guid = T1.RepoObject_guid;
